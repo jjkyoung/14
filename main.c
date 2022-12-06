203a31775e7dd2bb71dd6aa5cc86029fed45c31a
newykk226 <ykk226@naver.com>
2022-12-06 16:33:02 +0900

    02

--- main.c
+++ main.c
@@ -3,24 +3,30 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	
-	char*pc;
-	int i;
-	
-	pc = (char*)malloc(100*sizeof(char));
-	if(pc == NULL)
-	{
-		printf("error");
-		return(-1);
-	}
-	
-	for(i=0;i<26;i++)
-		pc[i] = 'a' + i;
-	pc[i] = 0;
-	printf("%s\n", pc);
 
-	free(pc);
+struct Book{
+		int number;
+		char title[10];
+	};
 	
+	
+int main(int argc, char *argv[]) {
+	struct Book*p;
+	
+	p = (struct Book*)malloc(2*sizeof(struct Book));
+	
+	
+	if(p == NULL){
+		printf("메모리 할당 오류\n");
+		return -1;
+	}	
+	
+	p->number = 1;
+	strcpy(p->title, "C Programming");
+	
+	(p+1)->number = 2;
+	strcpy((p+1)->title, "Electronics");
+	
+	free(p);	
 	return 0;
 }
